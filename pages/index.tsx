2dd398ce46f38d04a376d671c569ddea7a13e7ff
Erwin Smit <Erwin.Smit@macaw.nl>
2021-03-17 16:31:34 +0100

    things

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import React from 'react'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <ul>
+          <li>
+            <Link href="/staticapi">Static render page</Link>
+          </li>
+          <li>
+            <Link href="/serverapi">Server side rendered page</Link>
+          </li>
+        </ul>
+      </main>
+    </div>
+  )
+}
---
+++ pages/serverapi.tsx
@@ -0,0 +1,26 @@
+export default function Server({meats, date}) {
+    return (
+        <div>
+            api called: {date}
+            <ul>
+                {meats.map((meat, i) => 
+                    <li key={i}>{meat}</li>
+                )}
+            </ul>
+        </div>
+    )
+}
+
+// static site generation...
+export async function getStaticProps() {
+    const res = await fetch('https://baconipsum.com/api/?type=meat-and-filler')
+    const meats = await res.json()
+    const now = new Date();
+
+    return {
+        props: {
+            meats,
+            date: `${now.toLocaleDateString()} - ${now.toLocaleTimeString()}`
+        },
+    }
+}
