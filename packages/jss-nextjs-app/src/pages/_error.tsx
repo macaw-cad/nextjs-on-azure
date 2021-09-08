6b2104231fb62f1cd56007475a936bddda677846
Erwin Smit <E.smit@intertrustgroup.com>
2021-09-08 17:01:13 +0200

    add more logging to error page

--- packages/jss-nextjs-app/src/pages/_error.tsx
+++ packages/jss-nextjs-app/src/pages/_error.tsx
@@ -3,22 +3,31 @@ import Head from 'next/head';
 
 interface ErrorPageProps {
   statusCode?: number | null | undefined;
+  res: any;
+  err: any;
 }
 
 /**
  * Rendered in case if we have 500 error, used only in Production mode
  * @link https://nextjs.org/docs/advanced-features/custom-error-page#customizing-the-error-page
  */
-const ErrorPage: NextPage<ErrorPageProps> = ({ statusCode }) => (
+const ErrorPage: NextPage<ErrorPageProps> = ({ statusCode, res, err }) => (
   <>
     <Head>
       <title>Error</title>
     </Head>
     <div style={{ padding: 10 }}>
       <p>
-        {statusCode ? `An error ${statusCode} occurred op de server` : 'An error occurred on client'}
+        {statusCode ? `An error ${statusCode} occurred on the server` : 'An error occurred on client'}
+      </p>
+      <p>
+         {JSON.stringify(res)}
+      </p>
+      <p>
+        {JSON.stringify(err)}
       </p>
       <a href="/">Go to the Home page</a>
+
     </div>
   </>
 );
@@ -26,7 +35,7 @@ const ErrorPage: NextPage<ErrorPageProps> = ({ statusCode }) => (
 ErrorPage.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
 
-  return { statusCode };
+  return { statusCode, res, err };
 };
 
 export default ErrorPage;
