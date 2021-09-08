b8f8b09ecb6aac667d0e2eba477c23a7eb4b22f3
Erwin Smit <E.smit@intertrustgroup.com>
2021-09-08 11:29:19 +0200

    ...

--- packages/jss-nextjs-app-azure-functions/jss-nextjs-server/index.js
+++ packages/jss-nextjs-app-azure-functions/jss-nextjs-server/index.js
@@ -2,68 +2,114 @@ const fs = require("fs"); // Or `import fs from "fs";` with ESM
 const { URL } = require("url");
 let next;
 
+try {
+    const next0 = require("jss-nextjs-app-copy/node_modules/next");
+    console.log({next0});
+} catch(e) {
+    console.error(e);
+}
+
+try {
+    const next1 = require("./jss-nextjs-app-copy/node_modules/next");
+    console.log({
+        next1
+    })
+} catch(e) {
+    console.error(e);
+}
+
+try {
+    const next2 = require("../jss-nextjs-app-copy/node_modules/next");
+    console.log({
+        next2
+    })
+} catch(e) {
+    console.error(e);
+}
+
+
+try {
+    const next3 = require("../../jss-nextjs-app-copy/node_modules/next");
+    console.log({
+        next3
+    })
+} catch(e) {
+    console.error(e);
+}
+
+
+try {
+
+    const next4 = require("../../../jss-nextjs-app-copy/node_modules/next");
+    console.log({
+        next4
+    })
+} catch(e) {
+    console.error(e);
+}
+
 // Locally we get this dependency with lerna, on the server we use a different path
 console.log('fs.existsSync("./jss-nextjs-app-copy")', fs.existsSync("./jss-nextjs-app-copy"), fs.existsSync("jss-nextjs-app-copy"));
 console.log(fs.existsSync("./jss-nextjs-app-copy/node_modules/next"), fs.existsSync("jss-nextjs-app-copy/node_modules/next"));
 
-if (fs.existsSync("jss-nextjs-app-copy")) {
-    next = require("./jss-nextjs-app-copy/node_modules/next");
-} else {
-    next = require("@nextjsonazure/jss-nextjs-app/node_modules/next")
-}
+// if (fs.existsSync("jss-nextjs-app-copy")) {
+//     next = require("./jss-nextjs-app-copy/node_modules/next");
+// } else {
+//     next = require("@nextjsonazure/jss-nextjs-app/node_modules/next")
+// }
 
-next = require("../../jss-nextjs-app/node_modules/next");
-console.log("next", next);
+// next = require("../../jss-nextjs-app/node_modules/next");
+// console.log("next", next);
 
-const dev = false;
-let app;
-let handle;
+// const dev = false;
+// let app;
+// let handle;
 
-module.exports = async function (context, req) {
-    const path = (req?.params?.remainingPath && req?.params?.remainingPath !== "nextjsserver") ? `/${req?.params?.remainingPath}` : "/index"
+// module.exports = async function (context, req) {
+//     const path = (req?.params?.remainingPath && req?.params?.remainingPath !== "nextjsserver") ? `/${req?.params?.remainingPath}` : "/index"
     
-    if (!app) {
-        try {
-            app = next({ 
-                dev,
-                conf: {
-                    /* 
-                        This flag is required to prevent the "implicit header is not a function" error
-                        see: https://www.gitmemory.com/issue/zeit/next.js/8407/522278225
+//     if (!app) {
+//         try {
+//             app = next({ 
+//                 dev,
+//                 conf: {
+//                     /* 
+//                         This flag is required to prevent the "implicit header is not a function" error
+//                         see: https://www.gitmemory.com/issue/zeit/next.js/8407/522278225
 
-                        Because gzipping can and should be enabled on the CDN this is not a problem:
-                        https://docs.microsoft.com/en-us/azure/cdn/cdn-improve-performance
-                    */
-                    compress: false,
-                    // for local use, TODO: put in env:
-                    distDir: fs.existsSync("jss-nextjs-app-copy") ? "./jss-nextjs-app-copy/.next" : "../jss-nextjs-app/.next",
-                }
-            });
+//                         Because gzipping can and should be enabled on the CDN this is not a problem:
+//                         https://docs.microsoft.com/en-us/azure/cdn/cdn-improve-performance
+//                     */
+//                     compress: false,
+//                     // for local use, TODO: put in env:
+//                     distDir: fs.existsSync("jss-nextjs-app-copy") ? "./jss-nextjs-app-copy/.next" : "../jss-nextjs-app/.next",
+//                 }
+//             });
 
-            await app.prepare();
-            handle = app.getRequestHandler();
-        } catch(e) {
-            console.error(e);
-            context.res = {
-                status: 500,
-                body: path + JSON.stringify(e)
-            };
-        }
-    }
+//             await app.prepare();
+//             handle = app.getRequestHandler();
+//         } catch(e) {
+//             console.error(e);
+//             context.res = {
+//                 status: 500,
+//                 body: path + JSON.stringify(e)
+//             };
+//         }
+//     }
 
-    const parsedUrl = new URL(`https://randombaseurl.nl${path}`);
+//     const parsedUrl = new URL(`https://randombaseurl.nl${path}`);
 
-    // This fixes the "__nextlocale of undefined" bug
-    parsedUrl.query = {};
+//     // This fixes the "__nextlocale of undefined" bug
+//     parsedUrl.query = {};
     
-    try {
-        await handle(req, context.res, parsedUrl);
-    } catch(e) {
-        console.error(e);
+//     try {
+//         await handle(req, context.res, parsedUrl);
+//     } catch(e) {
+//         console.error(e);
 
-        context.res = {
-            status: 500,
-            body: path + JSON.stringify(e)
-        };
-    }
-}
+//         context.res = {
+//             status: 500,
+//             body: path + JSON.stringify(e)
+//         };
+//     }
+// }
