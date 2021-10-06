1031e507abb3e6dbcf8a28c079e8ecc1ccf163e6
Erwin Smit <E.smit@intertrustgroup.com>
2021-10-06 12:47:36 +0200

    adding bootstrap nav

--- packages/jss-nextjs-app/src/components/navigation/Navigation.tsx
+++ packages/jss-nextjs-app/src/components/navigation/Navigation.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { ComponentProps } from 'lib/component-props';
+import React from 'react';
 
 type Result = {
   pageTitle?: {
@@ -11,6 +12,7 @@ type Result = {
   children: {
     results: Result[];
   };
+  id: string;
 };
 
 type Data = {
@@ -44,34 +46,72 @@ const Navigation: React.FC<NavigationProps> = ({ fields }): JSX.Element => {
           </a>
         </Link>
       </h5>
-      <nav className="my-2 my-md-0 mr-md-3">
-        {fields.data.item.children.results.map((child, i) => {
-          return child.pageTitle && child.url?.path ? (
-            <li key={i}>
-              <Link href={child.url.path}>
-                <a className="p-2 text-dark">{child.pageTitle.value}</a>
-              </Link>
 
-              {child.children.results.length > 0 && (
-                <ul>
-                  {child.children.results.map((grandChild, index) => {
-                    if (!grandChild.pageTitle || !grandChild.url) {
-                      return null;
-                    }
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            {fields.data.item.children.results.map((child) => {
+              if (!child.pageTitle && child.url?.path) {
+                return null;
+              }
 
-                    return (
-                      <li key={index}>
-                        <Link href={grandChild.url.path}>
-                          <a className="p-2 text-dark">{grandChild.pageTitle.value}</a>
+              return child.pageTitle && child.url?.path ? (
+                <React.Fragment key={child.id}>
+                  {child.children.results.length > 0 ? (
+                    <li className="nav-item dropdown">
+                      <a
+                        className={`nav-link ${
+                          child.children.results.length > 0 ? 'dropdown-toggle' : ''
+                        }`}
+                        href="#"
+                        role="button"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                      >
+                        {child.pageTitle.value}
+                      </a>
+
+                      <div className="dropdown-menu">
+                        <Link href={child.url.path}>
+                          <a className="dropdown-item">{child.pageTitle.value}</a>
                         </Link>
-                      </li>
-                    );
-                  })}
-                </ul>
-              )}
-            </li>
-          ) : null;
-        })}
+                        {child.children.results.map((grandChild) => {
+                          if (!grandChild.pageTitle || !grandChild.url) {
+                            return null;
+                          }
+
+                          return (
+                            <Link href={grandChild.url.path} key={grandChild.id}>
+                              <a className="dropdown-item">{grandChild.pageTitle.value}</a>
+                            </Link>
+                          );
+                        })}
+                      </div>
+                    </li>
+                  ) : (
+                    <li className="nav-item">
+                      <Link href={child.url.path}>
+                        <a className="nav-link">{child.pageTitle.value}</a>
+                      </Link>
+                    </li>
+                  )}
+                </React.Fragment>
+              ) : null;
+            })}
+          </ul>
+        </div>
       </nav>
     </div>
   );
